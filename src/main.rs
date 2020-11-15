25d528a8f43ba9289b4a8ea85263d20f1b5ee2c7
Nikolay Danailov <frostblooded@yahoo.com>
2020-11-15 12:36:13 +0200

    Improve initialization to make program faster

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use rand::seq::SliceRandom;
-use rand::Rng;
 use std::io::stdin;
+
+macro_rules! timeit {
+    ($s: expr, $x: expr) => {{
+        let start = std::time::Instant::now();
+        let res = $x;
+        let duration = start.elapsed();
+        println!("{} took {:?}", $s, duration);
+        res
+    }};
+}
 
 #[derive(Debug)]
 struct Board {
@@ -32,42 +41,46 @@ impl Board {
         self.init_queens();
 
         for i in 0..self.n {
-            let queen_row = self.queens[i as usize];
-
-            let main_diag_index = i - queen_row + self.n - 1;
-            self.main_diag_collis[main_diag_index as usize] += 1;
-
-            let secondary_diag_index = i + queen_row;
-            self.secondary_diag_collis[secondary_diag_index as usize] += 1;
-
-            self.row_collis[queen_row as usize] += 1;
+            self.change_collision_for_queen(i as usize, 1);
         }
     }
 
+    #[inline]
     fn init_queens(&mut self) {
         self.queens = vec![];
+        let mut rng = rand::thread_rng();
 
         for i in 0..self.n {
-            self.queens.push(self.get_min_row_for_queen(i as usize));
+            self.queens.push(
+                *self
+                    .get_min_rows_for_queen(i as usize)
+                    .choose(&mut rng)
+                    .unwrap(),
+            );
         }
     }
 
+    #[inline]
     fn get_queen_main_diag_index(&self, queen_idx: usize) -> usize {
         self.get_pos_main_diag_index(queen_idx, self.queens[queen_idx] as usize)
     }
 
+    #[inline]
     fn get_queen_secondary_diag_index(&self, queen_idx: usize) -> usize {
         self.get_pos_secondary_diag_index(queen_idx, self.queens[queen_idx] as usize)
     }
 
+    #[inline]
     fn get_pos_main_diag_index(&self, col_idx: usize, row_idx: usize) -> usize {
-        col_idx - row_idx + (self.n - 1) as usize
+        col_idx + (self.n - 1) as usize - row_idx
     }
 
+    #[inline]
     fn get_pos_secondary_diag_index(&self, col_idx: usize, row_idx: usize) -> usize {
         col_idx + row_idx
     }
 
+    #[inline]
     fn get_pos_collisions(&self, col_idx: usize, row_idx: u32) -> u32 {
         self.main_diag_collis[self.get_pos_main_diag_index(col_idx, row_idx as usize)]
             + self.secondary_diag_collis
@@ -75,6 +88,7 @@ impl Board {
             + self.row_collis[row_idx as usize]
     }
 
+    #[inline]
     fn get_queen_collisions(&self, queen_idx: usize) -> u32 {
         self.get_pos_collisions(queen_idx, self.queens[queen_idx])
     }
@@ -82,6 +96,7 @@ impl Board {
     fn get_all_max_queens(&self) -> Vec<usize> {
         let mut max_val = None;
         let mut max_queens = vec![];
+        max_queens.reserve(self.n as usize);
 
         for i in 0..(self.n as usize) {
             let curr_val = self.get_queen_collisions(i);
@@ -100,6 +115,7 @@ impl Board {
         max_queens
     }
 
+    #[inline]
     fn get_max_queen(&self) -> usize {
         *self
             .get_all_max_queens()
@@ -120,6 +136,7 @@ impl Board {
             (self.row_collis[self.queens[idx] as usize] as i32 + val) as u32;
     }
 
+    #[inline]
     fn move_queen(&mut self, queen_idx: usize, row_idx: u32) {
         self.change_collision_for_queen(queen_idx, -1);
         self.queens[queen_idx] = row_idx;
@@ -144,6 +161,24 @@ impl Board {
         }
 
         true
+    }
+
+    fn get_min_rows_for_queen(&self, queen_idx: usize) -> Vec<u32> {
+        let mut min_rows = vec![];
+        let mut min_rows_val = None;
+
+        for i in 0..self.n {
+            let val = self.get_pos_collisions(queen_idx, i);
+
+            if min_rows_val.is_none() || min_rows_val.unwrap() > val {
+                min_rows = vec![i];
+                min_rows_val = Some(val);
+            } else if min_rows_val.unwrap() == val {
+                min_rows.push(i);
+            }
+        }
+
+        min_rows
     }
 
     fn get_min_row_for_queen(&self, queen_idx: usize) -> u32 {
@@ -173,6 +208,7 @@ impl Board {
             i += 1;
 
             if i >= 2 * self.n {
+                println!("Restart");
                 self.init();
                 self.solve();
             }
@@ -218,5 +254,5 @@ fn main() {
 
     dbg!(&board);
 
-    // println!("{}", board.to_pretty_string());
+    //println!("{}", board.to_pretty_string());
 }
